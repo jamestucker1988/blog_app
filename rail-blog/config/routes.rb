9cf3d6ce2c5e7f50b661cca648dfcfe431275d58
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-01-22 10:01:03 +0100

    delete routes, view and home controller

--- rail-blog/config/routes.rb
+++ rail-blog/config/routes.rb
@@ -1,9 +1,5 @@
 Rails.application.routes.draw do
-<<<<<<< HEAD
   get "/articles", to: "articles#index"
-=======
-  get 'home/index'
->>>>>>> 8ab45917db023d71b9b01e02dfc403ed30bc7873
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'home#index'
 end
