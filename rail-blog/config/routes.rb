7bf9ea71cd2f4179ff051848f42cde1a6a4ef61c
Enekwechi chinonso gerald <chiiinonsky0009@gmail.com>
2021-01-22 11:18:54 +0100

    add link to the index page

--- rail-blog/config/routes.rb
+++ rail-blog/config/routes.rb
@@ -1,6 +1,5 @@
 Rails.application.routes.draw do
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  # root 'home#index'
+  root "articles#index"
+
+  resources :articles
 end
